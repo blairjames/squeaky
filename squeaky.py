a1659dcaead2ae676d2972cb7bca5c22ec11b859
root <blair@blairjames.com>
2018-05-28 00:25:49 +1000

    refactor, add argument for length, add num core detection

--- squeaky.py
+++ squeaky.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
 
 #Squeaky takes any wordlist and cleans out dodgey characters, short words and duplicates.
-
+import argparse
+import os
 import time
 import multiprocessing
-from typing import Generator, List
-import argparse
+from typing import Generator
 #TODO: Point at dir and let her rip, clean duplicates, do gen vs list
 
 
@@ -18,6 +18,7 @@ class Squeaky:
         self.input_files = "/root/SecLists/Passwords/bt4-password.txt"
         self.output_file = "/root/SecLists/Passwords/consolidated.txt"
         self.min_word_length = 6
+        self.procs: int = (multiprocessing.cpu_count() * 4)
 
     def get_file_length(self, file: str) -> int:
         t1 = time.perf_counter()
@@ -49,23 +50,28 @@ class Squeaky:
                     self.write_to_output(z)
                 else:
                     continue
-            except UnicodeDecodeError as a:
+            except UnicodeDecodeError:
                 continue
         return True
 
-    def parse_arguments(self):
-        new_args = argparse.ArgumentParser()
-        new_args.add_argument("Input file")
-        new_args.add_argument("output file")
-        new_args.add_argument("-d", "--dir", help="Input directory of wordlists")
-        new_args.add_argument("-z", "--procs", help="Number of Processes")
-        parsed_args = new_args.parse_args()
-        return parsed_args
-
-    def clear_screen(self):
+    def parse_arguments(self) -> argparse.ArgumentParser:
         try:
-            import os
+            new_args = argparse.ArgumentParser()
+            new_args.add_argument("Input file")
+            new_args.add_argument("output file")
+            new_args.add_argument("-d", "--dir", help="Input a directory of wordlists instead of single file.")
+            new_args.add_argument("-l", "--len", help="Minimum word length, words shorter than "
+                                                      "specified length will be cleansed.")
+            new_args.add_argument("-z", "--procs", help="Number of processes to supercharge processing.")
+            parsed_args = new_args.parse_args()
+            return parsed_args
+        except Exception as e:
+            print("Error!! in parse_arguments: " + str(e))
+
+    def clear_screen(self) -> bool:
+        try:
             os.system("clear")
+            return True
         except Exception as e:
             print("Error! in clearscreen: " + str(e))
 
@@ -76,7 +82,7 @@ class Squeaky:
         print("  _(__  )/ /_/ // /_/ //  __/ /_/ /_  ,<  _  /_/ / ")
         print("  /____/ \__, / \__,_/ \___/\__,_/ /_/|_| _\__, /  ")
         print("           /_/                            /____/   \n\n")
-
+        print("     Lean, Mean, Word List Cleaning Machine\n\n")
 
 def main():
     t1 = time.perf_counter()
@@ -85,7 +91,7 @@ def main():
     new_squeaky.bruce()
     new_squeaky.parse_arguments()
     words = new_squeaky.read_file(new_squeaky.input_files)
-    with multiprocessing.Pool(processes=32) as pool:
+    with multiprocessing.Pool(processes = new_squeaky.procs) as pool:
         pool.map(new_squeaky.process_word, words)
     t2 = time.perf_counter()
     print("\nTotal processing time: " + str(t2 - t1) + " sec")
