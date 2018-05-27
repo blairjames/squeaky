f2e7ec8990ff8429c9ed05018b60fe76f39e9ac8
root <blair@blairjames.com>
2018-05-27 15:34:10 +1000

    made OO

--- squeaky.py
+++ squeaky.py
@@ -1,64 +1,69 @@
 #!/usr/bin/env python3
 
-#Squeaky takes any wordlist and cleans out dodgey characters and short words#
+#Squeaky takes any wordlist and cleans out dodgey characters and short words.
 
 import time
 import multiprocessing
-from typing import Generator
+from typing import Generator, List
 
-
+#TODO: Point at dir and let her rip, clean duplicates
 # Set your paths and min word length here:
-original_file = "/root/SecLists/Passwords/"
+original_file = "/root/SecLists/Passwords/darkweb2017-top10000.txt"
 new_file = "/root/SecLists/Passwords/consolidated.txt"
 min_word_length = 6
 
+class Squeaky:
 
-def get_file_length(file: str) -> int:
-    i :int = 0
-    wrds = read_file_comprehen(file)
-    for w in wrds:
-        i += 1
-    return i
+    def __init__(self):
+        self.dir_to_enum = "/root/SecLists/Passwords/"
+        self.new_file = "/root/SecLists/Passwords/consolidated.txt"
+        self.min_word_length = 6
 
+    def get_file_length(self, file: str) -> int:
+        wrds = self.read_file(file)
+        length = sum(1 for w in wrds)
+        print(str(length))
+        return length
 
-def read_file_comprehen(path_to_file: str) -> Generator:
-    with open(path_to_file, "rb") as file:
-        file_list = ([line] for line in file.readlines())
-    return file_list
+    def read_file(self, path_to_file: str) -> List:
+        t1 = time.perf_counter()
+        with open(path_to_file, "rb") as file:
+            file_list = list([line] for line in file.readlines())
+        t2 = time.perf_counter()
+        print(str(t2 - t1))
+        return file_list
 
+    def write_new_file(self, word: str):
+        t1 = time.perf_counter()
+        with open(new_file, "a") as file:
+            file.write(word)
+        t2 = time.perf_counter()
+        print(str(t2 - t1))
 
-def write_new_file(word: str):
-    with open(new_file, "a") as file:
-        file.write(word)
-
-
-def go(word):
-
-    for x in word:
-        try:
-            z = str(x.decode("utf-8"))
-            if not (len(z) < (min_word_length + 1)):
-                write_new_file(z)
-            else:
+    def go(self, word):
+        for x in word:
+            try:
+                z = str(x.decode("utf-8"))
+                if not (len(z) < (min_word_length + 1)):
+                    self.write_new_file(z)
+                else:
+                    continue
+            except UnicodeDecodeError as a:
                 continue
-        except UnicodeDecodeError as a:
-            continue
-
 
 def main():
 
     t1 = time.perf_counter()
-    words = read_file_comprehen(original_file)
+    new_squeaky = Squeaky()
+
+    words = new_squeaky.read_file(original_file)
     with multiprocessing.Pool(processes=32) as pool:
-        pool.map(go, words)
+        pool.map(new_squeaky.go, words)
 
-    ridgy = get_file_length(original_file)
-    newy = get_file_length(new_file)
-    diff = (ridgy - newy)
+    ridgy = new_squeaky.get_file_length(original_file)
 
-    print("Number of words in original file: " + str(ridgy))
-    print("Number of words in new file: " + str(newy))
-    print("Words Removed: " + str(diff))
+    print("Number of words in input file: " + str(ridgy))
+
     t2 = time.perf_counter()
     print("\nTotal processing time: " + str(t2 - t1) + " sec")
 
